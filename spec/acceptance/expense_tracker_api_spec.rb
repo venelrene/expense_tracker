ce66f72768902056464d92b6282afd2828abacd2
venelrene <venelrene@gmail.com>
2018-08-18 11:24:30 -0400

    rackup! hook our application up to a web server so we can actually see it working.

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -24,7 +24,6 @@ module ExpenseTracker
 		include Rack::Test::Methods
 
 		it 'records submitted expenses' do
-			pending 'Need to persist expenses'
 			coffee = post_expense(
 					'payee' => 'Starbucks',
 					'amount' => 5.75,
